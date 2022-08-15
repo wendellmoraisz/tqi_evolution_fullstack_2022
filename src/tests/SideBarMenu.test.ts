8f16d864dcc19df5a67b1edd49ff64ac7a11d750
Wendell <wendellmorais3319@gmail.com>
2022-08-15 17:48:23 -0300

    test: criando testes do side menu

---
+++ src/tests/SideBarMenu.test.ts
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import SideBarMenu from "../components/sideMenu";
+
+describe("Sidebar Component", () => {
+    it("should render a list of items", () => {
+        const { getByText } = render(SideBarMenu());
+
+        expect(getByText("Lançar venda")).toBeInTheDocument();
+        expect(getByText("Cadastrar livro")).toBeInTheDocument();
+        expect(getByText("Fazer pedido")).toBeInTheDocument();
+    });
+});
