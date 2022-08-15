4e53999754752f4bec939e7833dc3e80afcec933
Wendell <wendellmorais3319@gmail.com>
2022-08-15 17:47:56 -0300

    feat: side menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import SideBarMenu from './components/sideMenu';
 
 function App() {
   return (
-    <div>ola</div>
+    <SideBarMenu/>
   );
 }
 
---
+++ src/components/sideMenu/index.tsx
@@ -0,0 +1,17 @@
+
+
+const SideBarMenu = () => {
+
+    return (
+        <nav>
+            <h1>TQI Books</h1>
+            <div>
+                <li>Lançar venda</li>
+                <li>Cadastrar livro</li>
+                <li>Fazer pedido</li>
+            </div>
+        </nav>
+    )
+}
+
+export default SideBarMenu;
