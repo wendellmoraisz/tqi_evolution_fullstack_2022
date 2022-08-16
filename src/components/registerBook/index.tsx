342cf0c62d69a00fc411cc544c746ff3537563f2
Wendell <wendellmorais3319@gmail.com>
2022-08-16 11:08:18 -0300

    feat: limpar formulário depois de confirmar

--- src/components/registerBook/index.tsx
+++ src/components/registerBook/index.tsx
@@ -4,13 +4,22 @@ import Book from "../../types/Book";
 
 const RegisterBook = () => {
 
-    const [inputValues, setInputValues] = useState<Book>({
+    const inputDefaultValue = {
         title: "",
         author: "",
         publishingCompany: "",
         bookImgSrc: "",
-        publicationYear: "",
-    });
+        publicationYear: 0,
+    }
+
+    const [inputValues, setInputValues] = useState<Book>(inputDefaultValue);
+
+    const clearInputs = () => setInputValues(inputDefaultValue);
+
+    const addBook = () => {
+        registerBook(inputValues);
+        clearInputs();
+    }
 
     const handleInputValue = (eventTarget: React.ChangeEvent<HTMLInputElement>,
         stateProperty: "title" | "author" | "publishingCompany" | "bookImgSrc" | "publicationYear"
@@ -23,29 +32,35 @@ const RegisterBook = () => {
             <div>
                 <input
                     type="text"
+                    value={inputValues.title}
                     onChange={e => handleInputValue(e, "title")}
                     placeholder="título"
                 />
                 <input
                     type="text"
+                    value={inputValues.author}
                     onChange={e => handleInputValue(e, "author")}
                     placeholder="autor"
                 />
                 <input
                     type="text"
+                    value={inputValues.publishingCompany}
                     onChange={e => handleInputValue(e, "publishingCompany")}
                     placeholder="editora"
                 />
                 <input
-                    type="text" onChange={e => handleInputValue(e, "bookImgSrc")}
+                    type="text" 
+                    value={inputValues.bookImgSrc}
+                    onChange={e => handleInputValue(e, "bookImgSrc")}
                     placeholder="url da imagem do livro"
                 />
                 <input
-                    type="text"
+                    type="number"
+                    value={inputValues.publicationYear}
                     onChange={e => handleInputValue(e, "publicationYear")}
                     placeholder="ano de publicação"
                 />
-                <button onClick={() => registerBook(inputValues)}>confirmar</button>
+                <button onClick={addBook}>confirmar</button>
             </div>
         </div>
     )
