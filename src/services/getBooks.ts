a1b1a889e1f95187c9a318b105a50c78186d7b48
Wendell <wendellmorais3319@gmail.com>
2022-08-20 11:33:05 -0300

    refactor: definindo tipo de retorno dos serviços de busca

--- src/services/getBooks.ts
+++ src/services/getBooks.ts
@@ -1,4 +1,6 @@
-const getBooks = async () => {
+import BookSale from "../types/BookSale";
+
+const getBooks = async (): Promise<BookSale[]> => {
     const result = await fetch("http://localhost:8080/livros");
     const data = await result.json();
     return data;
--- src/services/getClients.ts
+++ src/services/getClients.ts
@@ -1,4 +1,6 @@
-const getClients = async () => {
+import Client from "../types/Client";
+
+const getClients = async (): Promise<Client[]> => {
     const response = await fetch("http://localhost:8080/clientes");
     const data = await response.json();
     return data;
