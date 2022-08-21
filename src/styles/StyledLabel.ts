6814237de7e9136d9d55bd23f13760e6ea902581
Wendell <wendellmorais3319@gmail.com>
2022-08-21 09:37:31 -0300

    style: criando campo estilizado

---
+++ src/styles/StyledLabel.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const StyledLabel = styled.label`
+    height: 30px;
+    width: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px 0;
+    border-radius: 5px;
+    cursor: pointer;
+    padding: 2px 10px;
+    transition: all .3s ease;
+`;
+
+export default StyledLabel;
